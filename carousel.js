3f116c13aed2993fb2a25fe935c2c80ad11d655c
RoyalSoup <68163606+RoyalSoup@users.noreply.github.com>
2023-06-22 19:25:22 -0500

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,68 +1,89 @@
-let currentIndex = 0;
-let items = document.querySelectorAll('.carousel-item');
-let itemCount = items.length;
+var images = [
+  {
+    src: "images/image1.jpg",
+    orientation: "landscape"
+  },
+  {
+    src: "images/image2.jpg",
+    orientation: "portrait"
+  },
+  {
+    src: "images/image3.jpg",
+    orientation: "portrait"
+  },
+  {
+    src: "images/image4.jpg",
+    orientation: "landscape"
+  },
+];
 
-const cycleItems = () => {
-    for (let i = 0; i < itemCount; i++) {
-        items[i].style.display = 'none';
-    }
-    items[currentIndex].style.display = 'block';
-}
-
-const prevItem = () => {
-    currentIndex -= 1;
-    if (currentIndex < 0) currentIndex = itemCount - 1;
-    cycleItems();
+var currentIndex = {
+  "portrait": 0,
+  "landscape": 0
 };
 
-const nextItem = () => {
-    currentIndex += 1;
-    if (currentIndex >= itemCount) currentIndex = 0;
-    cycleItems();
-};
+var currentOrientation = window.innerWidth > window.innerHeight ? "landscape" : "portrait";
 
-document.addEventListener('keydown', (event) => {
-    switch (event.code) {
-        case "ArrowLeft": // Left arrow key
-            prevItem();
-            break;
-        case "ArrowRight": // Right arrow key
-            nextItem();
-            break;
-    }
-});
-
-document.getElementById('carousel').addEventListener('click', (event) => {
-    if (event.clientX < window.innerWidth / 2) {
-        prevItem();
-    } else {
-        nextItem();
-    }
-});
-
-const checkOrientation = () => {
-    // Determine whether the device is likely to be a mobile device based on its width
-    const isMobile = window.matchMedia("(max-width: 767px)").matches;
-    const orientation = screen.width > screen.height ? "landscape" : "portrait";
-    if (isMobile) {
-        items = document.querySelectorAll(`.carousel-item.${orientation}, .carousel-item.square`);
-    } else {
-        items = document.querySelectorAll('.carousel-item');
-    }
-    itemCount = items.length;
-    currentIndex = 0;
-    cycleItems();
-
-    // Check device pixel ratio and adjust the font size of h1
-    const pixelRatio = window.devicePixelRatio;
-    const h1 = document.querySelector('h1');
-    if (pixelRatio > 1 && isMobile) {
-        h1.style.fontSize = '2.5em';
-    } else {
-        h1.style.fontSize = '1.2em';
-    }
+function filterImages() {
+  if (window.innerWidth > 767) {
+    return images;
+  } else {
+    return images.filter(image => image.orientation === currentOrientation);
+  }
 }
 
-window.addEventListener('resize', checkOrientation);
-window.addEventListener('orientationchange', checkOrientation);
-checkOrientation();
+function changeImage(event, isNext) {
+  var filteredImages = filterImages();
+  var carouselImage = document.querySelector('.carousel-image');
+
+  if (isNext) {
+    currentIndex[currentOrientation]++;
+  } else {
+    currentIndex[currentOrientation]--;
+  }
+
+  if (currentIndex[currentOrientation] < 0) {
+    currentIndex[currentOrientation] = filteredImages.length - 1;
+  } else if (currentIndex[currentOrientation] >= filteredImages.length) {
+    currentIndex[currentOrientation] = 0;
+  }
+
+  carouselImage.src = filteredImages[currentIndex[currentOrientation]].src;
+}
+
+function displayCurrentImage() {
+  var filteredImages = filterImages();
+  var carouselImage = document.querySelector('.carousel-image');
+  carouselImage.src = filteredImages[currentIndex[currentOrientation]].src;
+}
+
+// Click events for navigation
+document.querySelector('.carousel').addEventListener('click', function(event) {
+  var isNext = true;
+
+  // Check if clicked on arrow
+  if (event.target.classList.contains('arrow-left')) {
+    isNext = false;
+  } else if (event.target.classList.contains('arrow-right')) {
+    isNext = true;
+  } else {
+    // If not clicked on arrow, calculate based on click position
+    var rect = event.currentTarget.getBoundingClientRect();
+    var x = event.clientX - rect.left; //x position within the carousel
+    isNext = x > rect.width / 2;
+  }
+  
+  changeImage(event, isNext);
+});
+
+// Monitor for screen orientation change
+window.addEventListener("resize", function() {
+  var newOrientation = window.innerWidth > window.innerHeight ? "landscape" : "portrait";
+  if (newOrientation !== currentOrientation) {
+    currentOrientation = newOrientation;
+    displayCurrentImage(); // refresh the image
+  }
+});
+
+// Initial load
+displayCurrentImage();
